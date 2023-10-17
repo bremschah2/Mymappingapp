6a58d703511d371fbd4831c38f639764871f8e9d
bremschah2 <chahb021@gmail.com>
2023-10-17 23:58:37 +0000

    APIs removed

--- mapproject/mapproject/settings.py
+++ mapproject/mapproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 import os
 
 
@@ -23,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -143,9 +143,3 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 SESSION_COOKIE_SECURE = False
 
 
-
-
-MAPBOX_TOKEN = config('')
-SECRET_KEY = config('')
-API_KEY = config('')
-ACCESS_KEY = config('')
